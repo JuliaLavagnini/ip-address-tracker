36725f352e98a4040c5df78a83e4e7090d4930b9
Julia Lavagnini Maia <65735429+JuliaLavagnini@users.noreply.github.com>
2024-08-21 00:22:15 +0100

    Add leafjs API to map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var iconLocation = L.icon({
+    iconUrl: 'assets/media/icon-location.svg',
+    iconSize:     [46, 56], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+});
+
+L.marker([51.5, -0.09], {icon: iconLocation}).addTo(map);
